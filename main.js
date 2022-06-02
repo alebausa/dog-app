1ff1dce163bd02bb0b017ed1b1512dd986dd9017
Ale Bausa <alebausa@MacBook-Pro.home>
2022-06-02 12:19:26 +0200

    step 1

--- main.js
+++ main.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /* Routes */
+app.get('/', (req, res, next) => {
+  res.render('index');
+})
 
-
-
-app.listen(3000);
+app.listen(process.env.PORT, () => console.log(`Connected to port ${process.env.PORT}`));
