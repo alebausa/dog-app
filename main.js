7693b3dc69e1dd0d1b49b6aa0839a3bb955df321
Ale Bausa <alebausa@MacBook-Pro.home>
2022-06-02 12:47:00 +0200

    step 2

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const { response } = require('express');
 require('dotenv').config();
 
 app.set("views", __dirname + "/views");
@@ -11,6 +12,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /* Routes */
 app.get('/', (req, res, next) => {
   res.render('index');
+})
+
+app.get('/search', async (req, res, next) => {
+  const { breed } = req.query;
+  const error = `No dogs found by ${breed}`;
+  try {
+    const response = await axios.get(`https://dog.ceo/api/breed/${breed}/images`)
+    console.log(response.data.status)
+    res.render('results', { query: breed, breeds: response.data.message });
+  } catch (err) {
+    res.render('index', { error });
+  }
 })
 
 app.listen(process.env.PORT, () => console.log(`Connected to port ${process.env.PORT}`));
